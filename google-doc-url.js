ec74ef261b750efd982cf6b20080f702343ac079
melissa2021hmr <89110018+melissa2021hmr@users.noreply.github.com>
2021-11-04 17:57:39 -0600

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1mZ9r-PNF_4p7A4yVcjN8DJ-2vJPV-QlGNXtFrnZ0Lto/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyCQjD5TjR9Zy2ElWH9bn4e7iZk2X0ZtZiU';
